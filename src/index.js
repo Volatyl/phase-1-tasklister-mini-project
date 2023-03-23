816e496dde53604a359f49e3b07fdc08bdbb220e
Kennedy Muinde <kennedymuinde@gmail.com>
2023-03-23 21:46:26 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const taskInput = document.getElementById("new-task-description");
+  const tasksOutput = document.getElementById("tasks");
+  const submitBtn = document.getElementById("submitBtn");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const toDo = taskInput.value;
+    const task = document.createElement("li");
+    task.innerHTML = `${toDo} <button id=delBtn>X</button>`;
+    tasksOutput.appendChild(task);
+    taskInput.value = "";
+
+    const delBtn = task.querySelector("#delBtn");
+
+    delBtn.addEventListener("click", () => {
+      delBtn.parentNode.remove();
+    });
+  });
 });
